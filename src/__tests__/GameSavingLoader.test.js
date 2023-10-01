3b08b59c3c5280bc6a3541954b4653d0464f5731
Tatyana Amosova <atgatg@bk.ru>
2023-10-01 22:13:12 +0300

    added test

--- src/__tests__/GameSavingLoader.test.js
+++ src/__tests__/GameSavingLoader.test.js
@@ -1,6 +1,6 @@
 import GameSavingLoader from '../GameSavingLoader';
 
-test('testing GameSavingLoader.load()', async () => {
+test('testing GameSavingLoader.load()-1', async () => {
   const data = await GameSavingLoader.load();
   expect(data).toEqual({
     id: 9,
@@ -9,4 +9,20 @@ test('testing GameSavingLoader.load()', async () => {
       id: 1, name: 'Hitman', level: 10, points: 2000,
     },
   });
+});
+
+test('testing GameSavingLoader.load()-2', (done) => {
+  GameSavingLoader.load()
+    .then((saving) => {
+      const expectedData = {
+        id: 9,
+        created: 1546300800,
+        userInfo: {
+          id: 1, name: 'Hitman', level: 10, points: 2000,
+        },
+      };
+      expect(saving).toEqual(expectedData);
+      done();
+    })
+    .catch(console.error)
 });
